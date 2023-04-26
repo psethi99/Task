ae47a35b26527a503106eca0e53d1c510effdc0a
Prakhil Sethi <62275108+psethi99@users.noreply.github.com>
2023-04-26 19:01:36 +0530

    Update home_screen.dart

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:final_review/Screens/Offline_users.dart';
+import 'package:final_review/Screens/Online_users.dart';
 import 'package:final_review/Screens/tab_1.dart';
 import 'package:flutter/material.dart';
 import '../models/EmployeeBloc.dart';
@@ -47,9 +49,9 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
           // Widgets for Tab 1
           EmployeeList(employeeBloc: employeeBloc),
           // Widgets for Tab 2
-          Center(child: Text('Tab 2')),
+          OnlineUserList(),
           // Widgets for Tab 3
-          Center(child: Text('Tab 3')),
+          OfflineUserList(),
         ],
       ),
     );
